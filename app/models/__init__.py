6361c96377638640e43d700b2f06d5888b318da6
leengyen4 <leengyen5@gmail.com>
2025-03-05 14:15:22 -0800

    added models into models/init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .board import Board
+from .pin import Pin
+from .boardPin import BoardPin
+from .comment import Comment
+from .favorite import Favorite
