cb6a6de3db1ff542ab0a18d8bf32ce0d1a57bace
leengyen4 <leengyen5@gmail.com>
2025-03-07 12:59:17 -0800

    models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,6 @@ from .user import User
 from .db import environment, SCHEMA
 from .board import Board
 from .pin import Pin
-from .boardPin import BoardPin
 from .comment import Comment
 from .favorite import Favorite
+from .boardPin import BoardPin
